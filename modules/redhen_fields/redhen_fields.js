66bb3773861a0ff440af7ab2f8a476cc74c5b994
Michael Prasuhn <mike@mikeyp.net>
2011-03-02 03:12:37 -0800

    Make default items work properly, setup to start on Javascript interface for adding/removing items

--- modules/redhen_fields/redhen_fields.js
+++ modules/redhen_fields/redhen_fields.js
@@ -1,21 +1,30 @@
 (function ($) {
 
 
-/**
- * Field instance settings screen: force the 'Display on registration form'
- * checkbox checked whenever 'Required' is checked.
- */
-Drupal.behaviors.redhenFieldsDisplayWidget = {
-  attach: function (context, settings) {
-    $('.redhen-email-widget-item').each(function(index, Element) {
-      var text;
-      if ($(this).find('.form-type-textfield input').attr('value') === '') {
-        $(this).hide();
+  /**
+   * Field instance settings screen: force the 'Display on registration form'
+   * checkbox checked whenever 'Required' is checked.
+   */
+  Drupal.behaviors.redhenFieldsDisplayWidget = {
+    attach: function (context, settings) {
+      $('.redhen-email-widget-item').each(function(index, element) {
+        if ($(element).find('.form-type-textfield input').attr('value') === '') {
+          $(element).hide();
+        }
+
+
+      });
+    }
+  };
+
+  Drupal.redhenFieldsAddItem = function () {
+    $('.redhen-email-widget-item').each(function(index, element) {
+      if ($(element).is(":visible") == false) {
+        $(element).show();
+        return false;
       }
     });
-  }
-};
-
+  };
 
 
 })(jQuery);
