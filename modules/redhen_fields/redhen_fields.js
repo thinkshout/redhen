a9b101024c61cb59717f87d76b9cae7c75a73646
Michael Prasuhn <mike@mikeyp.net>
2011-03-07 11:39:28 -0700

    Adding bulk and on hold fields to email settings, and fixing display. JS needs alot of work still

--- modules/redhen_fields/redhen_fields.js
+++ modules/redhen_fields/redhen_fields.js
@@ -8,10 +8,15 @@
   Drupal.behaviors.redhenFieldsDisplayWidget = {
     attach: function (context, settings) {
       Drupal.behaviors.redhenFieldsDisplayWidget.hidden = 0;
+      Drupal.behaviors.redhenFieldsDisplayWidget.shown = 0;
       $('.redhen-email-widget-item').each(function(index, element) {
+
         if ($(element).find('.form-type-textfield input').attr('value') === '') {
           Drupal.behaviors.redhenFieldsDisplayWidget.hidden++;
           $(element).hide();
+        }
+        else {
+          Drupal.behaviors.redhenFieldsDisplayWidget.shown++;
         }
         $(element).find('.remove').bind('click', function () {
           $(element).find('.form-text').attr('value', '');
@@ -21,12 +26,23 @@
             Drupal.redhenFieldsDefaultFirstItem();
           }
           Drupal.behaviors.redhenFieldsDisplayWidget.hidden++;
+          Drupal.behaviors.redhenFieldsDisplayWidget.shown--;
           if (Drupal.behaviors.redhenFieldsDisplayWidget.hidden > 0) {
             $('.field-widget-redhen-email-widget .add-another').show();
+          }
+          if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
+            $('.field-widget-redhen-email-widget .legend').show();
+          } else {
+            $('.field-widget-redhen-email-widget .legend').hide();
           }
           return false;
         });
       });
+      if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
+        $('.field-widget-redhen-email-widget .legend').show();
+      } else {
+        $('.field-widget-redhen-email-widget .legend').hide();
+      }
       
       $('.field-widget-redhen-email-widget .add-another').bind('click', function() {
         Drupal.redhenFieldsAddItem();
@@ -40,8 +56,14 @@
       if ($(element).is(":visible") == false) {
         $(element).show();
         Drupal.behaviors.redhenFieldsDisplayWidget.hidden--;
+        Drupal.behaviors.redhenFieldsDisplayWidget.shown++;
         if (Drupal.behaviors.redhenFieldsDisplayWidget.hidden == 0) {
           $('.field-widget-redhen-email-widget .add-another').hide();
+        }
+        if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
+          $('.field-widget-redhen-email-widget .legend').show();
+        } else {
+          $('.field-widget-redhen-email-widget .legend').hide();
         }
         return false;
       }
