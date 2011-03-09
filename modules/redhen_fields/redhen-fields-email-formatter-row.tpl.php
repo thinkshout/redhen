7ce4a4aa32c62ce6b0acd5882295fc5fd7b9b149
Michael Prasuhn <mike@mikeyp.net>
2011-03-09 15:06:13 -0600

    Make email field formatter work properly, and update default contact display

---
+++ modules/redhen_fields/redhen-fields-email-formatter-row.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Print the template for a single row of the redhen email widget formatter.
+ */
+
+dsm(get_defined_vars());
+?>
+
+<div class="<?php print implode(' ', $classes_array); ?>">
+  <div class="email-label-section">
+    <div class="email-address"><?php print check_plain($item['value']) ?></div>
+    <?php print check_plain($item['label']) ?>
+  </div>
+  <div class="bulk-label"><?php print $item['bulk'] ?></div>
+  <div class="hold-label"><?php print $item['hold'] ?></div>
+  <div class="primary-label"><?php print $item['default'] ?></div>
+
+</div>
