afde0f6f381c714f70ec7db5523935342deb57eb
Michael Prasuhn <mike@mikeyp.net>
2011-03-09 19:19:58 -0600

    Make basic display of contacts work

--- modules/redhen_contact/redhen-contact.tpl.php
+++ modules/redhen_contact/redhen-contact.tpl.php
@@ -5,3 +5,23 @@
  */
 
 ?>
+<div id="redhen-contact-<?php print $contact->contact_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="field property-first-name field-label-inline clearfix">
+    <div class="field-label"><?php print $first_name_label; ?>&nbsp;</div>
+    <div class="field-items">
+      <div class="field-item even"><?php print $first_name; ?></div>
+    </div>
+  </div>
+
+  <div class="field property-last-name field-label-inline clearfix">
+    <div class="field-label"><?php print $last_name_label; ?>&nbsp;</div>
+    <div class="field-items">
+      <div class="field-item even"><?php print $last_name; ?></div>
+    </div>
+  </div>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print render($content); ?>
+  </div>
+</div>
+
