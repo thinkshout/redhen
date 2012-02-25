22370e74e2fa578e7fab395e7f033de48ce7ab29
Lev Tsypin <lev@thinkshout.com>
2012-02-24 16:14:09 -0800

    Refactoring email field to use a more traditional approach.

--- modules/redhen_fields/redhen_fields.js
+++ modules/redhen_fields/redhen_fields.js
@@ -1,82 +1,19 @@
 (function ($) {
 
-
   /**
-   * Field instance settings screen: force the 'Display on registration form'
-   * checkbox checked whenever 'Required' is checked.
+   * Ensure only a single default is selected.
    */
   Drupal.behaviors.redhenFieldsDisplayWidget = {
     attach: function (context, settings) {
-      Drupal.behaviors.redhenFieldsDisplayWidget.hidden = 0;
-      Drupal.behaviors.redhenFieldsDisplayWidget.shown = 0;
-      $('.redhen-email-widget-item').each(function(index, element) {
-
-        if ($(element).find('.form-type-textfield input').attr('value') === '') {
-          Drupal.behaviors.redhenFieldsDisplayWidget.hidden++;
-          $(element).hide();
-        }
-        else {
-          Drupal.behaviors.redhenFieldsDisplayWidget.shown++;
-        }
-        $(element).find('.remove').bind('click', function () {
-          $(element).find('.form-text').attr('value', '');
-          $(element).hide();
-          $(element).appendTo( $('.field-widget-redhen-email-widget') );
-          if ($(element).find('.form-radio').attr('checked') == true) {
-            Drupal.redhenFieldsDefaultFirstItem();
+      $('input[name*="default"]', $('.field-type-redhen-email')).click(function() {
+        var $checked = $(this);
+        $('input[name*=default]', $('.field-type-redhen-email')).each(function() {
+          if ($(this).attr('id') !== $checked.attr('id')) {
+            $(this).removeAttr('checked');
           }
-          Drupal.behaviors.redhenFieldsDisplayWidget.hidden++;
-          Drupal.behaviors.redhenFieldsDisplayWidget.shown--;
-          if (Drupal.behaviors.redhenFieldsDisplayWidget.hidden > 0) {
-            $('#redhen-email-add-another').show();
-          }
-          if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
-            $('#redhen-email-widget-legend').show();
-          } else {
-            $('#redhen-email-widget-legend').hide();
-          }
-          return false;
         });
-      });
-      if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
-        $('#redhen-email-widget-legend').show();
-      } else {
-        $('#redhen-email-widget-legend').hide();
-      }
-      
-      $('#redhen-email-add-another').bind('click', function() {
-        Drupal.redhenFieldsAddItem();
-        return false;
       });
     }
   };
-
-  Drupal.redhenFieldsAddItem = function () {
-    $('.redhen-email-widget-item').each(function(index, element) {
-      if ($(element).is(":visible") == false) {
-        $(element).show();
-        Drupal.behaviors.redhenFieldsDisplayWidget.hidden--;
-        Drupal.behaviors.redhenFieldsDisplayWidget.shown++;
-        if (Drupal.behaviors.redhenFieldsDisplayWidget.hidden == 0) {
-          $('#redhen-email-add-another').hide();
-        }
-        if (Drupal.behaviors.redhenFieldsDisplayWidget.shown > 0) {
-          $('#redhen-email-widget-legend').show();
-        } else {
-          $('#redhen-email-widget-legend').hide();
-        }
-        return false;
-      }
-    });
-  };
-
-  Drupal.redhenFieldsDefaultFirstItem = function () {
-    $('.redhen-email-widget-item').each(function(index, element) {
-      if ($(element).is(":visible") == true) {
-        $(element).find('.form-radio').attr('checked', true);
-      }
-    });
-  };
-
 
 })(jQuery);
