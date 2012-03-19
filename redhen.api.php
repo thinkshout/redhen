4368d373a391912eae6ed2aff54a502aba3e0bcc
Lev Tsypin <lev@thinkshout.com>
2012-03-19 10:58:20 -0600

    Update api docs.

--- redhen.api.php
+++ redhen.api.php
@@ -3,6 +3,13 @@
  * @file
  * Hooks provided by the RedHen contact module.
  */
+
+/**
+ * All RedHen custom entities, including contacts, organizations, memberships,
+ * notes, and engagements are based on Entity API Entity controller class and
+ * inherit all of it's CRUD hooks. It also provides the following additional
+ * hooks.
+ */
  
 /**
  * Prevent a contact record from being deleted.
@@ -15,50 +22,6 @@ function hook_redhen_contact_can_delete($contact) {
 }
 
 /**
- * Allows you to prepare contact data before it is saved.
- *
- * @param $contact
- *   The contact object to be saved.
- * @return void
- */
-function hook_redhen_contact_presave(&$contact) {
-
-}
-
-/**
- * undocumented function
- *
- * @param object $contact 
- *   The contact object being inserted.
- * @return void
- */
-function hook_redhen_contact_insert($contact) {
-  
-}
-
-/**
- * undocumented function
- *
- * @param object $contact 
- *   The contact object being updated.
- * @return void
- */
-function hook_redhen_contact_update($contact) {
-  
-}
-
-/**
- * undocumented function
- *
- * @param string $contact
- *   The contact object being deleted.
- * @return void
- */
-function hook_redhen_contact_delete($contact) {
-  
-}
-
-/**
  * Alter the display name for a contact.
  *
  * @param string $name
@@ -67,4 +30,19 @@ function hook_redhen_contact_delete($contact) {
  */
 function hook_redhen_contact_name_alter(&$name, RedhenContact $contact) {
   return $contact->last_name . ', ' . $contact->last_name;
+}
+
+/**
+ * Provide a form API element exposed as a RedHen setting.
+ *
+ * @return array
+ */
+function hook_redhen_settings() {
+  return array(
+    'redhen_contact_connect_users' => array(
+      '#type' => 'checkbox',
+      '#title' => t('Connect users to Redhen contacts'),
+      '#description' => t('If checked, Redhen will attempt to connect Drupal users to Redhen contacts by matching email addresses when a contact is updated.'),
+      '#default_value' => variable_get('redhen_contact_connect_users', FALSE)
+  ));
 }
