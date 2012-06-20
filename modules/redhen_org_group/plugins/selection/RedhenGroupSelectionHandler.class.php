2560386c95ce34d08ec05e21c4b38b456fa4f002
Lev Tsypin <lev@thinkshout.com>
2012-06-20 16:18:32 -0700

    Reverted to implementing EntityReference_SelectionHandler rather than extending EntityReference_SelectionHandler_Generic. Ref: #2597.

--- modules/redhen_org_group/plugins/selection/RedhenGroupSelectionHandler.class.php
+++ modules/redhen_org_group/plugins/selection/RedhenGroupSelectionHandler.class.php
@@ -4,7 +4,7 @@
 /**
  * RedHenGroup selection handler.
  */
-class RedhenGroupSelectionHandler extends EntityReference_SelectionHandler_Generic {
+class RedhenGroupSelectionHandler implements EntityReference_SelectionHandler {
 
   protected function __construct($field, $instance) {
     $this->field = $field;
@@ -80,4 +80,36 @@ class RedhenGroupSelectionHandler extends EntityReference_SelectionHandler_Gener
   public static function settingsForm($field, $instance) {
     return array();
   }
+
+  /**
+   * Implements EntityReferenceHandler::validateAutocompleteInput().
+   */
+  public function validateAutocompleteInput($input, &$element, &$form_state, $form) {
+      $entities = $this->getReferencableEntities($input, '=', 6);
+      if (empty($entities)) {
+	// Error if there are no entities available for a required field.
+	form_error($element, t('There are no entities matching "%value"', array('%value' => $input)));
+      }
+      elseif (count($entities) > 5) {
+	// Error if there are more than 5 matching entities.
+	form_error($element, t('Many entities are called %value. Specify the one you want by appending the id in parentheses, like "@value (@id)"', array(
+	  '%value' => $input,
+	  '@value' => $input,
+	  '@id' => key($entities),
+	)));
+      }
+      elseif (count($entities) > 1) {
+	// More helpful error if there are only a few matching entities.
+	$multiples = array();
+	foreach ($entities as $id => $name) {
+	  $multiples[] = $name . ' (' . $id . ')';
+	}
+	form_error($element, t('Multiple entities match this reference; "%multiple"', array('%multiple' => implode('", "', $multiples))));
+      }
+      else {
+	// Take the one and only matching entity.
+	return key($entities);
+      }
+  }
+
 }
