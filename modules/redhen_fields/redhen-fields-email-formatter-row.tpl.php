33f1f617908094c3cf5bcef873d927112115cce7
Lev Tsypin <lev@thinkshout.com>
2013-01-02 20:33:21 -0800

    Fix array index error when an email label was empty.

--- modules/redhen_fields/redhen-fields-email-formatter-row.tpl.php
+++ modules/redhen_fields/redhen-fields-email-formatter-row.tpl.php
@@ -8,7 +8,7 @@
 
 <div class="<?php print implode(' ', $classes_array); ?>">
   <div class="email-address"><?php print check_plain($item['value']) ?></div>
-  <div class="email-label"><?php print check_plain($item['label']) ?></div>
+  <div class="email-label"><?php print $label ?></div>
   <div class="bulk-label"><?php print $bulk ?></div>
   <div class="hold-label"><?php print $hold ?></div>
   <div class="primary-label"><?php print $default ?></div>
