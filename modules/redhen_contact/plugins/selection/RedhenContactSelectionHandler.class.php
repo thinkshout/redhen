893d6ac8a3c4ca9a790cd807cefcfcec2c30a1e6
nadavoid <nadavoid@89512.no-reply.drupal.org>
2015-11-20 09:30:25 -0800

    Issue #2431083 by nadavoid: Autocomplete filter for contacts to match on first and last name

--- modules/redhen_contact/plugins/selection/RedhenContactSelectionHandler.class.php
+++ modules/redhen_contact/plugins/selection/RedhenContactSelectionHandler.class.php
@@ -12,16 +12,18 @@ class EntityReference_SelectionHandler_Generic_redhen_contact extends EntityRefe
    * Build an EntityFieldQuery to get referencable entities.
    */
   protected function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    // @todo Enable filtering by first OR last name. This will require adding
-    // metadata and/or tags to the query, altering it using a
-    // hook_query_TAG_alter(), and using a db_or() to generate the OR condition.
+    $query = parent::buildEntityFieldQuery($match, $match_operator);
+
+    // Filtering by first and last name. EFQs do not support OR conditions, so
+    // a tag is added, which allows the resulting query to be altered. In that
+    // query_alter, all of the conditions are added, so they are not set here.
     // See http://drupal.stackexchange.com/questions/14499/using-or-with-entityfieldquery
     // and https://api.drupal.org/api/drupal/includes!database!database.inc/function/db_or/7
     // and http://www.phase2technology.com/blog/or-queries-with-entityfieldquery/.
-    // For now, the filter is just on first name.
-    $query = parent::buildEntityFieldQuery($match, $match_operator);
-
-    $query->propertyCondition('first_name', $match, $match_operator);
+    $query->addTag('redhen_contact_generic_selection');
+    // Add a 'search_string' metadata so the query_alter can easily find the
+    // search text.
+    $query->addMetaData('search_string', $match);
 
     return $query;
   }
