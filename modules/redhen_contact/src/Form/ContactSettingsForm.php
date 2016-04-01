122911ba422a2bda84d9d27bd2ab4cadbcc666b8
Alex Ellison <alex.ellison@thinkshout.com>
2016-03-31 17:20:55 -0700

    Port of global contact settings form
    Instead of appending the global contact settings to the general
    settings at /admin/config/redhen/ we’ve broken the general and contact
    settings out into two separate forms: /admin/config/redhen/general and
    /admin/config/redhen/contact.

--- modules/redhen_contact/src/Form/ContactSettingsForm.php
+++ modules/redhen_contact/src/Form/ContactSettingsForm.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\redhen_contact\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -17,7 +17,8 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @ingroup redhen_contact
  */
-class ContactSettingsForm extends FormBase {
+class ContactSettingsForm extends ConfigFormBase {
+
   /**
    * Returns a unique string identifying the form.
    *
@@ -26,6 +27,13 @@ class ContactSettingsForm extends FormBase {
    */
   public function getFormId() {
     return 'contact_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['redhen_contact.settings'];
   }
 
   /**
@@ -40,7 +48,6 @@ class ContactSettingsForm extends FormBase {
     // Empty implementation of the abstract submit class.
   }
 
-
   /**
    * Defines the settings form for Contact entities.
    *
@@ -53,8 +60,88 @@ class ContactSettingsForm extends FormBase {
    *   Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['contact_settings']['#markup'] = 'Settings form for Contact entities. Manage field settings here.';
-    return $form;
-  }
+    $config = $this->config('redhen_contact.settings');
 
+    $form = array(
+      'unique_email' => array(
+        '#type' => 'checkbox',
+        '#title' => t('Require contacts to have a valid email address'),
+        '#description' => t('Controls the contact form validation. Must be true to enable Drupal user connections keyed on email.'),
+        '#default_value' => redhen_contact_email_unique(),
+      ),
+      'connect_users' => array(
+        '#type' => 'checkbox',
+        '#title' => t('Connect users to RedHen contacts'),
+        '#description' => t('If checked, RedHen will attempt to connect Drupal users to RedHen contacts by matching email addresses when a contact is updated.'),
+        '#default_value' => $config->get('connect_users'),
+        '#states' => array(
+          'enabled' => array(
+            ':input[name="contact_unique_email"]' => array('checked' => TRUE),
+          ),
+        ),
+      ),
+      'embed_on_user_form' => array(
+        '#type' => 'checkbox',
+        '#title' => t('Embed RedHen Contact fields on the User edit form'),
+        '#description' => t('If checked, the RedHen Contact tab on users will be removed, and the Contact edit fields will instead be attached to the bottom of the User Edit form.'),
+        '#default_value' => $config->get('embed_on_user_form'),
+        '#states' => array(
+          'enabled' => array(
+            ':input[name="contact_connect_users"]' => array('checked' => TRUE),
+          ),
+        ),
+      ),
+      'alter_username' => array(
+        '#type' => 'checkbox',
+        '#title' => t('Display username as contact label'),
+        '#description' => t("If checked, RedHen will alter the display of the Drupal username to match a linked contact's label."),
+        '#default_value' => $config->get('alter_username'),
+      ),
+      'registration' => array(
+        '#type' => 'fieldset',
+        '#title' => t('User registration'),
+        'settings' => array(
+          'registration' => array(
+            '#type' => 'checkbox',
+            '#options' => array(1, 1),
+            '#title' => t('Create a contact during user registration'),
+            '#default_value' => $config->get('registration'),
+          ),
+          'registration_types' => array(
+            '#type' => 'select',
+            '#options' => redhen_contact_type_options_list(),
+            '#title' => t('Allowed contact type'),
+            '#description' => t('Select the allowed contact types to create during registration. This can be overridden by appending the contact type machine name in the registration url.'),
+            '#default_value' => $config->get('registration_types'),
+            '#states' => array(
+              'invisible' => array(
+                ':input[name="contact_registration"]' => array('checked' => FALSE),
+              ),
+            ),
+          ),
+          'registration_link' => array(
+            '#type' => 'checkbox',
+            '#options' => array(1, 1),
+            '#title' => t('Link to existing contacts'),
+            '#description' => t('If a contact is found with the same email as the new Drupal user, it will be linked to the new account.'),
+            '#default_value' => $config->get('registration_link'),
+          ),
+          'registration_update' => array(
+            '#type' => 'checkbox',
+            '#options' => array(1, 1),
+            '#title' => t('Update contact fields'),
+            '#description' => t('When an existing contact is found and linked to, the submitted field values will overwrite the existing contact field values.'),
+            '#default_value' => $config->get('registration_update'),
+            '#states' => array(
+              'invisible' => array(
+                ':input[name="redhen_contact_reg_link"]' => array('checked' => FALSE),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
 }
