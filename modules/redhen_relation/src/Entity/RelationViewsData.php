619f2811565938a91082a9c9ded2ed4a7b35148b
Tauno Hogue <tauno@thinkshout.com>
2016-05-09 22:17:58 -0700

    Add related entity Views relations

--- modules/redhen_relation/src/Entity/RelationViewsData.php
+++ modules/redhen_relation/src/Entity/RelationViewsData.php
@@ -20,11 +20,41 @@ class RelationViewsData extends EntityViewsData implements EntityViewsDataInterf
   public function getViewsData() {
     $data = parent::getViewsData();
 
-    $data['redhen_relation']['table']['base'] = array(
-      'field' => 'id',
-      'title' => $this->t('Relation ID'),
-      'help' => $this->t('The Relation ID.'),
-    );
+    // Unset the default entity relationships.
+    // It does not work properly, the target type it is not defined.
+    unset($data['redhen_relation']['entity1']['relationship']);
+    unset($data['redhen_relation']['entity2']['relationship']);
+
+    // Collect all relationable entity types.
+    $relation_types = RelationType::loadMultiple();
+    $entity_type_ids = [];
+    /** @var \Drupal\redhen_relation\RelationTypeInterface $relation_type */
+    foreach ($relation_types as $relation_type) {
+      if ($entity_type_id = $relation_type->getEndpointEntityTypeId('1')) {
+        $entity_type_ids[] = $entity_type_id;
+      }
+      if ($entity_type_id = $relation_type->getEndpointEntityTypeId('2')) {
+        $entity_type_ids[] = $entity_type_id;
+      }
+    }
+    $entity_type_ids = array_unique($entity_type_ids);
+
+    // Provide a relationship for each entity type found.
+    foreach ($entity_type_ids as $entity_type_id) {
+      /** @var \Drupal\Core\Entity\EntityTypeInterface $entity_type */
+      $entity_type = $this->entityManager->getDefinition($entity_type_id);
+      $data['redhen_relation'][$entity_type_id] = [
+        'relationship' => [
+          'title' => $entity_type->getLabel(),
+          'help' => t('The related @entity_type.', ['@entity_type' => $entity_type->getLowercaseLabel()]),
+          'base' => $entity_type->getDataTable() ?: $entity_type->getBaseTable(),
+          'base field' => $entity_type->getKey('id'),
+          'relationship field' => 'related_entity',
+          'id' => 'standard',
+          'label' => $entity_type->getLabel(),
+        ],
+      ];
+    }
 
     return $data;
   }
