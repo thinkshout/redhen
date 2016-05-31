17cf00f98bce75546470a836cea2a331d477979d
Tauno Hogue <tauno@thinkshout.com>
2016-05-31 15:21:21 -0700

    Add default operation for roles under connection types

--- modules/redhen_connection/src/ConnectionTypeListBuilder.php
+++ modules/redhen_connection/src/ConnectionTypeListBuilder.php
@@ -9,6 +9,7 @@ namespace Drupal\redhen_connection;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a listing of Connection type entities.
@@ -19,6 +20,8 @@ class ConnectionTypeListBuilder extends ConfigEntityListBuilder {
    */
   public function buildHeader() {
     $header['label'] = $this->t('Connection type');
+    $header['endpoint_1'] = $this->t('Endpoint 1');
+    $header['endpoint_2'] = $this->t('Endpoint 2');
     $header['id'] = $this->t('Machine name');
     return $header + parent::buildHeader();
   }
@@ -28,9 +31,28 @@ class ConnectionTypeListBuilder extends ConfigEntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     $row['label'] = $entity->label();
+    $row['endpoint_1'] = $entity->getEndPointLabel(1);
+    $row['endpoint_2'] = $entity->getEndPointLabel(2);
     $row['id'] = $entity->id();
-    // You probably want a few more properties here...
     return $row + parent::buildRow($entity);
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultOperations(EntityInterface $entity) {
+    $operations = parent::getDefaultOperations($entity);
+
+    if ($entity->access('update') && $entity->hasLinkTemplate('edit-form')) {
+      $operations['roles'] = array(
+        'title' => $this->t('Roles'),
+        'weight' => 100,
+        'url' => $url = Url::fromRoute('entity.redhen_connection_role.collection', ['redhen_connection_type' => $entity->id()]),
+      );
+    }
+
+    return $operations;
+  }
+
 
 }
