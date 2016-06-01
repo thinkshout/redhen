82d47027a946b7011ad71eb620d8775b7d85166a
Tauno Hogue <tauno@thinkshout.com>
2016-06-01 09:00:19 -0700

    Attempt to limit listbuilder results based on route param

--- modules/redhen_connection/src/ConnectionRoleListBuilder.php
+++ modules/redhen_connection/src/ConnectionRoleListBuilder.php
@@ -9,6 +9,25 @@ use Drupal\Core\Entity\EntityInterface;
  * Provides a listing of Connection Role entities.
  */
 class ConnectionRoleListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+
+    $query = $this->getStorage()->getQuery()
+      ->sort($this->entityType->getKey('id'));
+
+    if ($connection_type = \Drupal::routeMatch()->getParameter('redhen_connection_type')) {
+      $query->condition('connection_type',$connection_type->id(),'=');
+    }
+
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    return $query->execute();
+  }
+
   /**
    * {@inheritdoc}
    */
