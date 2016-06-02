3482aa3d7429c1bdfb75f4ad95b6065021df566d
Tauno Hogue <tauno@thinkshout.com>
2016-06-01 23:33:19 -0700

    Rough form for role permissions

--- modules/redhen_connection/src/Form/ConnectionRoleForm.php
+++ modules/redhen_connection/src/Form/ConnectionRoleForm.php
@@ -5,6 +5,7 @@ namespace Drupal\redhen_connection\Form;
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\redhen_connection\ConnectionTypeInterface;
+use Drupal\redhen_connection\Entity\ConnectionType;
 
 /**
  * Class ConnectionRoleForm.
@@ -46,6 +47,49 @@ class ConnectionRoleForm extends EntityForm {
       ),
       '#disabled' => !$redhen_connection_role->isNew(),
     );
+
+    // Permissions.
+    /** @var ConnectionTypeInterface $connection_type */
+    $connection_type_id = $this->getEntityFromRouteMatch($this->getRouteMatch(), 'redhen_connection_type');
+    $connection_type = ConnectionType::load($connection_type_id);
+
+    // @todo Change getEndpointEntityTypeId to Ids and take no argument to get all endpoints.
+    $endpoints = [];
+    $endpoints[] = $connection_type->getEndpointEntityTypeId(1);
+    $endpoints[] = $connection_type->getEndpointEntityTypeId(2);
+
+    // At least one endpoint is a contact so we can have permissions.
+    if (in_array('redhen_contact', $endpoints)) {
+      $form['permissions'] = array(
+        '#type' => 'fieldset',
+        '#title' => $this->t('Permissions'),
+      );
+      $operations = ['view', 'view label', 'update', 'delete', 'create'];
+      // Connection permissions.
+      $form['permissions']['connection'] = array(
+        '#type' => 'checkboxes',
+        '#options' => $operations,
+        '#title' => $this->t('Connection'),
+        '#description' => $this->t('Applies to both the current user\'s connection and secondary connections. Sitewide permissions will override this setting.'),
+      );
+      // Other endpoint permissions.
+      // @todo hard coded, but shouldn't be.
+      $form['permissions']['redhen_org'] = array(
+        '#type' => 'checkboxes',
+        '#options' => $operations,
+        '#title' => $this->t('Entity'),
+        '#description' => $this->t('Sitewide permissions will override this setting.'),
+      );
+      // Connected Contact permissions.
+      $form['permissions']['redhen_contact'] = array(
+        '#type' => 'checkboxes',
+        '#options' => $operations,
+        '#title' => $this->t('Secondary Contact'),
+        '#description' => $this->t('A contact connected to the same entity via connection of the same type. Sitewide permissions will override this setting.'),
+      );
+
+    }
+
 
     return $form;
   }
