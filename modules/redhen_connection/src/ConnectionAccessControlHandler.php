df600afc94ec20f2af2f15b458e84556503db50a
Alex Ellison <alex.ellison@thinkshout.com>
2016-06-14 11:52:59 -0700

    Add bundle-specific connection permissions.
    Resolves #72

--- modules/redhen_connection/src/ConnectionAccessControlHandler.php
+++ modules/redhen_connection/src/ConnectionAccessControlHandler.php
@@ -30,20 +30,51 @@ class ConnectionAccessControlHandler extends EntityAccessControlHandler {
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     /** @var \Drupal\redhen_connection\ConnectionInterface $entity */
 
+    // Get Connection bundle.
+    $entity_bundle = $entity->getType();
+
     switch ($operation) {
       // @todo split out view label into its own permission.
       case 'view label':
       case 'view':
-        if (!$entity->isActive()) {
-          return AccessResult::allowedIfHasPermission($account, 'view inactive Connection entities');
+        // If Connection is active, check "view active" permissions to determine
+        // access.
+        if ($entity->isActive()) {
+          $view_access = AccessResult::allowedIfHasPermissions($account, [
+            'view active connection entities',
+            'view active ' . $entity_bundle . ' connection',
+          ], 'OR');
         }
-        return AccessResult::allowedIfHasPermission($account, 'view active Connection entities');
+        // If Connection is inactive, user needs "view inactive" permission to
+        // view.
+        else {
+          $view_access = AccessResult::allowedIfHasPermissions($account, [
+            'view inactive connection entities',
+            'view inactive ' . $entity_bundle . ' connection',
+          ], 'OR');
+        }
+
+        return $view_access;
 
       case 'update':
-        return AccessResult::allowedIfHasPermission($account, 'edit Connection entities');
+        // Check admin and bundle-specific edit permissions to determine
+        // edit access.
+        $edit_access = AccessResult::allowedIfHasPermissions($account, [
+          'edit connection entities',
+          'edit ' . $entity_bundle . ' connection',
+        ], 'OR');
+
+        return $edit_access;
 
       case 'delete':
-        return AccessResult::allowedIfHasPermission($account, 'delete Connection entities');
+        // Check admin and bundle-specific delete permissions to determine
+        // delete access.
+        $delete_access = AccessResult::allowedIfHasPermissions($account, [
+          'delete connection entities',
+          'delete ' . $entity_bundle . ' connection',
+        ], 'OR');
+
+        return $delete_access;
     }
 
     // Unknown operation, no opinion.
@@ -54,7 +85,19 @@ class ConnectionAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return AccessResult::allowedIfHasPermission($account, 'add Connection entities');
+    // If there is only one redhen_connection bundle, set $entity_bundle to it
+    // since ConnectionAddController::add returns the add form for the solitary
+    // bundle instead of a bundle select form if there is only one.
+    if (!$entity_bundle) {
+      $types = \Drupal::entityTypeManager()->getStorage('redhen_connection_type')->loadMultiple();
+      if ($types && count($types) == 1) {
+        $entity_bundle = array_keys($types)[0];
+      }
+    }
+    return AccessResult::allowedIfHasPermissions($account, [
+      'add connection entities',
+      'add ' . $entity_bundle . ' connection',
+    ], 'OR');
   }
 
 }
