bf723936d323f55bd56b0115cf7d825baa0bcbfe
Alex Ellison <alex.ellison@thinkshout.com>
2016-08-17 15:56:46 -0400

    Add subclass for getQuestion() function on all redhen entity delete forms.
    Subclass defines the delete question as “Are you sure you want to
    delete <entity bundle label> <entity label>?”
    
    This is better than the core entity module’s treatment where entity
    type and entity label are used because, for example, every redhen_org
    asks the user if they’d like to “delete the organization <org name”
    instead of saying “would you like to delete the <office|school|etc>
    <org name>”.

--- modules/redhen_org/src/Form/OrgDeleteForm.php
+++ modules/redhen_org/src/Form/OrgDeleteForm.php
@@ -8,6 +8,7 @@
 namespace Drupal\redhen_org\Form;
 
 use Drupal\Core\Entity\ContentEntityDeleteForm;
+use Drupal\redhen_org\Entity\OrgType;
 
 /**
  * Provides a form for deleting Org entities.
@@ -15,5 +16,15 @@ use Drupal\Core\Entity\ContentEntityDeleteForm;
  * @ingroup redhen_org
  */
 class OrgDeleteForm extends ContentEntityDeleteForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete the @org-type %name?', [
+      '@org-type' => OrgType::load($this->entity->bundle())->label(),
+      '%name' => $this->entity->label()
+    ]);
+  }
 
 }
